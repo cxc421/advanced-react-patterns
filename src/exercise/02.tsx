307db4e318e47da63898cd17e9e1532cc51b32a0
Chris Chuang <chrischuang0421@gmail.com>
2020-10-21 13:13:00 +0800

    Complete exercise 2 - Compound Components

---
+++ src/exercise/02.tsx
@@ -0,0 +1,72 @@
+// Compound Components
+// http://localhost:3000/isolated/exercise/02.tsx
+
+import React, {FC} from 'react';
+import {Switch} from '../switch';
+
+type ToggleOnOffProps = {on?: boolean};
+
+const ToggleOn: FC<ToggleOnOffProps> = ({on, children}) =>
+  on ? <>{children}</> : null;
+ToggleOn.displayName = `ToggleOn`;
+
+const ToggleOff: FC<ToggleOnOffProps> = ({on, children}) =>
+  !on ? <>{children}</> : null;
+
+type ToggleButtonInnerProps = {
+  on: boolean;
+  toggle: () => void;
+};
+
+const ToggleButtonInner: FC<ToggleButtonInnerProps> = ({on, toggle}) => (
+  <Switch on={on} onClick={() => toggle()} />
+);
+
+const ToggleButton: FC = () => null;
+
+const Toggle: FC = ({children}) => {
+  const [on, setOn] = React.useState(false);
+  const toggle = () => setOn(!on);
+
+  return (
+    <>
+      {React.Children.map(children, (child, index) => {
+        if (
+          React.isValidElement<ToggleOnOffProps>(child) &&
+          (child.type === ToggleOn || child.type === ToggleOff)
+        ) {
+          return React.cloneElement(child, {
+            on,
+          });
+        }
+        if (React.isValidElement(child) && child.type === ToggleButton) {
+          return (
+            <ToggleButtonInner
+              on={on}
+              toggle={toggle}
+              children={child.props.children}
+            />
+          );
+        }
+        if (React.isValidElement(child)) {
+          return child;
+        }
+      })}
+    </>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        {/* <span>Hello</span> */}
+        <ToggleButton />
+      </Toggle>
+    </div>
+  );
+}
+
+export default App;
