945819cdaddb63126deccbd54d1f51bd2887526b
Chris Chuang <chrischuang0421@gmail.com>
2020-10-22 22:26:21 +0800

    Complete exercise 4 - Props Collections and Getters

---
+++ src/exercise/04.tsx
@@ -0,0 +1,60 @@
+// Prop Collections and Getters
+// http://localhost:3000/isolated/exercise/04.js
+
+import React from 'react';
+import {Switch} from '../switch';
+
+function callAll(...fns: Function[]) {
+  return function (...args: any[]) {
+    fns.forEach(fn => fn && fn(...args));
+  };
+}
+
+function useToggle() {
+  const [on, setOn] = React.useState(false);
+  const toggle = () => setOn(!on);
+
+  return {
+    on,
+    toggle,
+    // add this one
+    togglerProps: {
+      'aria-pressed': on,
+      onClick: toggle,
+    },
+    // and this one
+    getTogglerProps({onClick, ...props}: any) {
+      return {
+        'aria-pressed': on,
+        onClick: callAll(onClick, toggle),
+        ...props,
+      };
+    },
+  };
+}
+
+function App() {
+  const {on, getTogglerProps} = useToggle();
+  return (
+    <div>
+      <Switch {...getTogglerProps({on})} />
+      <hr />
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
+        {on ? 'on' : 'off'}
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
